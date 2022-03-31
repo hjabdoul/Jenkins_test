12be2e913c02de3d75f2603ca6eaa12b4daa83b7
hjabdoul <102533892+hjabdoul@users.noreply.github.com>
2022-03-31 15:00:33 +0200

    Update apache_script.sh

--- apache_script.sh
+++ apache_script.sh
@@ -5,5 +5,5 @@ apt install -y apache2
 
 systemctl enable --now apache2
 hostnamectl set-hostname apache2_root
-echo "<h1>Je suis le plus fort "hostname"</h1>" > /var/www/html/index.html
+echo "<h1>Github "hostname"</h1>" > /var/www/html/index.html
 exit;

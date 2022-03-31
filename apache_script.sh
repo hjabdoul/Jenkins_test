9adb7844ff616ac7a902408e3475751535cca4e0
hjabdoul <102533892+hjabdoul@users.noreply.github.com>
2022-03-31 12:11:16 +0200

    Update apache_script.sh

--- apache_script.sh
+++ apache_script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-apt update -y
-apt install -y apache2
+sudo apt update -y
+sudo apt install -y apache2
 
-systemctl enable --now apache2
-hostnamectl set-hostname apache2_root
-echo "<h1>Je suis le plus fort "hostname"</h1>" > /var/www/html/index.html
-
-A$=(nano /var/www/html/index.html)
-
+sudo systemctl enable --now apache2
+sudo hostnamectl set-hostname apache2_root
+sudo echo "<h1>Je suis le plus fort "hostname"</h1>" > /var/www/html/index.html
 exit;
